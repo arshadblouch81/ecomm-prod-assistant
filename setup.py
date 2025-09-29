4c3d22de76b13bbe7ed710455d0b3ecd9450e53a
arshad abbas <arshadblouch81@gmail.com>
2025-09-29 06:07:31 +0500

    add first time

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecomm-prod-assistant",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies
+    ],
+)
---
+++ version.py
@@ -0,0 +1,38 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "python-dotenv",
+    "ipykernel",
+    "langchain_groq",
+    "langchain_google_genai",
+    "langchain-community",
+    "faiss-cpu",
+    "structlog",
+    "PyMuPDF",
+    "pylint",
+    "langchain-core",
+    "pytest",
+    "streamlit",
+    "fastapi",
+    "uvicorn",
+    "python-multipart",
+    "docx2txt",
+    "pypdf",
+    "pytesseract",
+    "pdfplumber",
+    "docx",
+    "python-pptx",
+    "deepeval",
+    "jinja2"
+    
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
+
+# # serve static & templates
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+# templates = Jinja2Templates(directory="../templates")
