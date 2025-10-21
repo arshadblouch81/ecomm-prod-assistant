5e64bf52f272e3dc59d7c5f087bf07d4b29fb3e1
arshad abbas <arshadblouch81@gmail.com>
2025-10-21 05:52:33 +0500

    changing directory name with underscore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ecomm-prod-assistant",
+    name="ecomm_prod_assistant",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
