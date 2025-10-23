85ca0c8e93622127aead69a1ede23ec9935fca69
arshad abbas <arshadblouch81@gmail.com>
2025-10-23 19:34:16 +0500

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python prod_assistant/mcp_servers/product_search_server.py &
+python prod_assistant/mcp_servers/product_search_server.py
 uvicorn prod_assistant.router.main:app --host 0.0.0.0 --port 8000 --workers 2
